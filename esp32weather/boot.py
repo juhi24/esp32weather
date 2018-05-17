962d08761abfcd9d64ea7691f1b2985fc47e0e47
Jussi Tiira <jussi@j24.fi>
2018-05-17 23:08:22 +0300

    wlan connection

---
+++ esp32weather/boot.py
@@ -0,0 +1,14 @@
+def connect():
+    import network
+    from secret import wlan_ssid, wlan_pass
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to ' + wlan_ssid)
+        sta_if.active(True)
+        sta_if.connect(wlan_ssid, wlan_pass)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+
+connect()
